b2cba25cff99c864d0ce1f005a7644db92a00d2f
saitejamandadi99 <saitejamandadi99>
2025-09-22 12:12:57 +0530

    created an api for fetching the details of the coins based on the marketcap and other details

---
+++ src/services/coinGeckoApi.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const BASE_URL = 'https://api.coingecko.com/api/v3';
+const API_KEY = process.env.NEXT_PUBLIC_COINGECKO_API_KEY; 
+
+
+const coinsGeckoAxiosApi = axios.create({
+    baseURL:BASE_URL,
+    headers:{
+    'x_cg_pro_api_key':API_KEY,
+    },
+})
+
+const getCoinGeckoCoins = async  () =>{
+    try{
+        const response = await coinsGeckoAxiosApi.get("/coins/markets",{
+            params:{
+                vs_currency:'usd', //price in currency
+                order:'market_cap_desc', //sorting in market cap descending (largest first)
+                per_page:10, // pagination per page number of coins.
+                page:1, // page of results to fetch.
+                price_change_percentage:'24h' //price change in percentage per 24hrs.
+            },
+        })
+        console.log(response.data)
+        return response.data
+    }
+    catch(err){
+        throw new Error(err.message || 'Error in fetching the coins. ');
+        
+    }
+}
+
+export default getCoinGeckoCoins
