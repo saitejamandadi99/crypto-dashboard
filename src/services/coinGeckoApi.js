fe5819e14ddddb27030a60f5894bc2dbee08a4d4
saitejamandadi99 <saitejamandadi99>
2025-09-22 18:33:07 +0530

    added api's for both trending and market coins

--- src/services/coinGeckoApi.js
+++ src/services/coinGeckoApi.js
@@ -4,7 +4,7 @@ const BASE_URL = 'https://api.coingecko.com/api/v3';
 const API_KEY = process.env.NEXT_PUBLIC_COINGECKO_API_KEY;
 
 
-const coinsGeckoAxiosApi = axios.create({
+export const coinsGeckoAxiosApi = axios.create({
     baseURL:BASE_URL,
     headers:{
     'x_cg_pro_api_key':API_KEY,
@@ -31,4 +31,35 @@ const getCoinGeckoCoins = async  (page = 1) =>{
     }
 }
 
-export default getCoinGeckoCoins
+export const getTrendingAxiosApi = async () =>{
+    try {
+        const response =await coinsGeckoAxiosApi.get('/search/trending')
+        return response.data.coins.map((c)=>c.item);
+        
+    } catch (err) {
+        throw new Error(err.message || 'Error in fetching the trending details')
+    }
+
+}
+
+export const getMarketCoinsAxiosApi = async () =>{
+    try{
+        const response = await coinsGeckoAxiosApi.get('/coins/markets',
+            {
+                params:{
+                vs_currency:'usd',
+                order:'market_cap_desc', 
+                per_page:10,
+                page:1, 
+                price_change_percentage:'24h',
+                ...params 
+            },
+            }
+        )
+
+    }
+    catch(err){
+        throw new Error(err.message || 'Error in fetching the market coins details')
+    }
+}
+
