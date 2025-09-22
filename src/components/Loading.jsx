5116968fe34f1ea574906c3a4faf79f8e37b11af
saitejamandadi99 <saitejamandadi99>
2025-09-22 12:39:34 +0530

    created a loading component for futher use using react-loader-spinner module

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import { TailSpin } from "react-loader-spinner";
+const LoadingComponent = () =>{
+    return(
+
+        <TailSpin height={80} width={80} color="blue" ariaLabel="loading"/>
+    )
+}
+
+
+export default LoadingComponent;
