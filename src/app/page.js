ac166562411aee04607b0a73f459bb572a7f434d
saitejamandadi99 <saitejamandadi99>
2025-09-22 14:11:56 +0530

    integrated the api call into the page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,33 @@
+import {useState,useEffect} from 'react'
+import CoinItem from '@/components/CoinItem'
+import LoadingComponent from '@/components/Loading'
+import getCoinGeckoCoins from '@/services/coinGeckoApi'
 
-import styles from "./page.module.css";
+const homePage = () => {
+    const [coinsList, setCoinsList] = useState([])
+    const [isLoading,setLoading] = useState(false)
+    const [error,setError] = useState(null)
+    const [success,setSuccess] = useState(null)
 
-export default function Home() {
-  return (
-    <div>
-      <h2>Welcome to the crypto web dashboard</h2>
-    </div>
-  );
+    useEffect(()=>{
+      setLoading(true)
+      setError(null)
+      setSuccess(null)
+
+      getCoinGeckoCoins()
+      .then((data)=>{
+        setCoinsList(data)
+        setSuccess('Data fetched Successfully')
+        setLoading(false)
+      })
+      .catch((err)=>{
+        setError(err.message)
+        setSuccess(null)
+        setLoading(false)
+      })
+
+    },[])
+    
 }
+
+export default homePage
